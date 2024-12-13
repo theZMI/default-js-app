f8d46bcc920fd0620c23b8cecb6807f27a941080
theZMI <zmigpost@gmail.com>
2024-12-13 17:08:39 +0200

    Пишем полный пусть до места куда скопировали приложение

--- auto-build-app/move-android-app.js
+++ auto-build-app/move-android-app.js
@@ -1,19 +1,20 @@
 const fs = require('fs');
 const { GetJsAppProjectDir, GetCordovaProjectDir } = require("./_helpers");
 
-const appDestination = GetJsAppProjectDir('dist-apps/');
 const config = JSON.parse(fs.readFileSync(GetJsAppProjectDir('package.json'), 'utf-8'));
+const destinationDir = GetJsAppProjectDir('dist-apps/');
+const destinationApk = destinationDir + config.version + '.apk';
 
 function CopyAndroidApp() {
-    if (!fs.existsSync(appDestination)){
-        fs.mkdirSync(appDestination);
+    if (!fs.existsSync(destinationDir)){
+        fs.mkdirSync(destinationDir);
     }
 
     fs.cpSync(
         GetCordovaProjectDir('platforms/android/app/build/outputs/apk/debug/app-debug.apk'),
-        appDestination + config.version + '.apk',
+        destinationApk,
     );
 }
 
 CopyAndroidApp();
-console.log(`5/5) copy android app to ${appDestination}: DONE`);
+console.log(`5/5) copy android app to ${destinationApk}: DONE`);
